677af02895dfc64c60ae33168d6c7a88dc3168c0
Daniel Lee <kokuyoku82@gmail.com>
2016-02-02 19:00:03 +0800

    Framework 1.3.7

---
+++ FaceKit/FaceKit.framework/Headers/FaceKit.h
@@ -0,0 +1,19 @@
+//
+//  FaceKit.h
+//  FaceKit
+//
+//  Created by 撒景賢 on 10/29/15.
+//  Copyright © 2015 Speed 3D Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FaceKit.
+FOUNDATION_EXPORT double FaceKitVersionNumber;
+
+//! Project version string for FaceKit.
+FOUNDATION_EXPORT const unsigned char FaceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceKit/PublicHeader.h>
+
+
