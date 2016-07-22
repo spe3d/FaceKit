fbe72b968aa6d50e3bd3cca36d9436cd75754cd3
w91379137 <w913791379137@gmail.com>
2016-07-22 11:49:52 +0800

    Add recorder manager

---
+++ FaceKit-Example/SPEAudioEngineManager.swift
@@ -0,0 +1,146 @@
+import Foundation
+import AVFoundation
+import FaceKit
+
+class SPEAudioEngineManager: NSObject {
+    
+    enum State: UInt {
+        case Default
+        case isRecording
+        case isPlaying
+    }
+    
+    dynamic private(set) var statusKVOKey : UInt = State.Default.rawValue
+    private(set) var status: State = .Default {
+        didSet {
+            statusKVOKey = status.rawValue
+        }
+    }
+    
+    var avatarController: FKAvatarController?
+    
+    let sampleRate = 22050
+    
+    private var audioEngine = AVAudioEngine()
+    private var outputFile = AVAudioFile()
+    
+    // MARK: - File URL
+    // URL for saved RecData
+    func recFileURL() -> NSURL {
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first! as String
+        let pathArray = [dirPath, "rec.aac"]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        return filePath!
+    }
+    
+    // remove file
+    func removeRecFile() {
+        let manager = NSFileManager.defaultManager()
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first! as NSURL
+        let path = url.URLByAppendingPathComponent("rec.aac").path!
+        if manager.fileExistsAtPath(path) {
+            try! manager.removeItemAtPath(path)
+        }
+    }
+    
+    // MARK:
+    func setup() {
+        // AudioSession
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setActive(true)
+        } catch let error as NSError  {
+            print("Error : \(error)")
+        }
+    }
+    
+    private var converterNode : AVAudioMixerNode?
+    func record() {
+        status = .isRecording
+        
+        self.setup()
+        self.avatarController?.startRecord(self.sampleRate)
+        
+        //Output Format
+        let intputFormat =
+            audioEngine.inputNode!.outputFormatForBus(0)
+        let outputFormat =
+            AVAudioFormat.init(commonFormat: intputFormat.commonFormat,
+                               sampleRate: Double(sampleRate),
+                               channels: AVAudioChannelCount(1),
+                               interleaved: false)
+        
+        var settings = outputFormat.settings
+        settings[AVFormatIDKey] = NSNumber(unsignedInt: kAudioFormatMPEG4AAC)
+        
+        // set outputFile
+        removeRecFile()
+        outputFile = try! AVAudioFile(forWriting: recFileURL(), settings: settings)
+        
+        //Convertor
+        if converterNode == nil {
+            converterNode = AVAudioMixerNode()
+            if let node = converterNode {
+                audioEngine.attachNode(node)
+                audioEngine.connect(audioEngine.inputNode!, to: node, format: audioEngine.inputNode!.outputFormatForBus(0))
+                audioEngine.connect(node, to: audioEngine.outputNode, format: outputFormat)
+            }
+        }
+        
+        converterNode?.installTapOnBus(0, bufferSize: 1024, format: converterNode?.outputFormatForBus(0)) { (buffer, when) in
+            
+            try! self.outputFile.writeFromBuffer(buffer)
+            
+            //avatarController write
+            let firstChannel = buffer.floatChannelData[0]
+            let array = Array(UnsafeBufferPointer(start: firstChannel, count: Int(buffer.frameLength)))
+            let doublesArray = array.map({ (value) -> Double in
+                return Double(value)
+            })
+            
+            //TODO: If buffer.floatChannelData is nil, or value not in range -1 ~ 1
+            _ = try? self.avatarController?.append(doublesArray)
+        }
+        
+        // AVAudioEngine start
+        if !audioEngine.running {
+            do {
+                try audioEngine.start()
+            } catch let error as NSError {
+                print("Couldn't start engine, \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func stopRecord() {
+        status = .Default
+        
+        // audioEngine stop
+        converterNode?.removeTapOnBus(0)
+        audioEngine.stop()
+        
+        print("Music Length:\(Float(self.outputFile.length) / Float(sampleRate)) s")
+        
+        let dict = try! NSFileManager.defaultManager().attributesOfItemAtPath(self.outputFile.url.path!)
+        print("File Size:\((dict[NSFileSize] as! Int) / 1024)KB")
+        
+        //TODO: If buffer.floatChannelData is nil, or value not in range -1 ~ 1
+        _ = try? self.avatarController?.stopRecord({
+            self.avatarController?.saveVoice(self.recFileURL().path!);
+        })
+    }
+    
+    func playRecData() {
+        status = .isPlaying
+        
+        self.avatarController?.startPlayVoiceAnimation({ (success) in
+            self.status = .Default
+        })
+    }
+    
+    func stopRecData() {
+        status = .Default
+        
+        self.avatarController?.stopPlayVoiceAnimation()
+    }
+}
---
+++ FaceKit-Example/SPERecorderManager.swift
@@ -0,0 +1,107 @@
+//
+//  SPERecorderManager.swift
+//  FaceKit
+//
+//  Created by w91379137 on 2016/7/12.
+//  Copyright © 2016年 Speed 3D Inc. All rights reserved.
+//
+
+import UIKit
+import FaceKit
+
+class SPERecorderManager: NSObject {
+
+    weak var recButton: UIButton? {
+        didSet {
+            reloadButtonsTitle()
+        }
+    }
+    weak var playButton: UIButton? {
+        didSet {
+            reloadButtonsTitle()
+        }
+    }
+    
+    private var context = 0
+    
+    var avatarController: FKAvatarController? {
+        didSet {
+            audioEngineMnager.avatarController = self.avatarController
+        }
+    }
+    
+    var audioEngineMnager = SPEAudioEngineManager()
+    
+    //MARK: - Init
+    override init() {
+        super.init()
+        audioEngineMnager.addObserver(self,
+                            forKeyPath: "statusKVOKey",
+                            options: [.Initial, .New],
+                            context: &context)
+    }
+    
+    deinit {
+        audioEngineMnager.removeObserver(self,
+                                         forKeyPath: "statusKVOKey",
+                                         context: &context)
+    }
+    
+    //MARK: - Observer
+    override func observeValueForKeyPath(keyPath: String?,
+                                         ofObject object: AnyObject?,
+                                                  change: [String : AnyObject]?,
+                                                  context: UnsafeMutablePointer<Void>) {
+        if context == context {
+            reloadButtonsTitle()
+        }
+        else {
+            super.observeValueForKeyPath(keyPath,
+                                         ofObject: object,
+                                         change: change,
+                                         context: context)
+        }
+    }
+    
+    func reloadButtonsTitle() {
+        switch audioEngineMnager.status {
+        case .Default:
+            playButton?.setTitle("Play Start", forState: .Normal)
+            recButton?.setTitle("Rec Start", forState: .Normal)
+            
+        case .isRecording:
+            playButton?.setTitle("Play x", forState: .Normal)
+            recButton?.setTitle("Rec Stop", forState: .Normal)
+            
+        case .isPlaying:
+            playButton?.setTitle("Play Stop", forState: .Normal)
+            recButton?.setTitle("Rec x", forState: .Normal)
+            break
+        }
+    }
+    
+    //MARK:
+    func recButtonPushed() {
+        if audioEngineMnager.status == .isPlaying { return }
+        
+        switch audioEngineMnager.status {
+        case .Default:
+            audioEngineMnager.record()
+        case .isRecording:
+            audioEngineMnager.stopRecord()
+        case .isPlaying: break
+        }
+    }
+    
+    func playButtonPushed() {
+        if audioEngineMnager.status == .isRecording { return }
+        
+        switch audioEngineMnager.status {
+        case .Default:
+            audioEngineMnager.playRecData()
+        case .isPlaying:
+            audioEngineMnager.stopRecData()
+        case .isRecording: break
+        }
+    }
+}
